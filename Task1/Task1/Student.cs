844158077f3e4fa75f13786cef6799b1698f619a
Mysh <tipomysh.ru@mail.ru>
2025-01-31 20:15:33 +0300

    done homework

---
+++ Task1/Task1/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double AvgMark { get; set;}
+
+    public Student(string _name, int _age, double _avgmark) 
+    {
+        Name = _name;
+        Age = _age;
+        AvgMark = _avgmark;
+    }    
+
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var products = new Dictionary<string, double>
+        {
+            {"Apple", 2.11 },
+            {"Banana", 4.15 },
+            {"Orange", 3.18 }
+        };
+
+        products["Kiwi"] = 5.61;
+
+        foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Key}: {product.Value} $");
+            }
+
+        Console.WriteLine("Введите продукт для поиска:");
+        var find = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(find))
+        {
+            Console.WriteLine("Некорректное значение");
+            find = Console.ReadLine();
+        }
+
+        if (products.ContainsKey(find))
+        {
+            double price = products[find];
+            Console.WriteLine($"{price}$");
+        }
+        else Console.WriteLine("Продукт не найден");
+
+        var keys = products.Keys.ToList();
+        foreach (var key in keys)
+        {
+            products[key] *= 1.1;
+        }
+
+        Console.WriteLine("Цена, увеличенная на 10%:");
+        foreach (var product in products)
+        {
+            Console.WriteLine($"{product.Key}: {product.Value} $");
+        }
+    }
+}
